0f40f84a29e0d4777089209fb20989919aac74f1
Pavan Gangadhara <pavan.gangadhara06@gmail.com>
2020-09-29 15:11:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import uuid from 'uuid'
+import "bootstrap/dist/css/bootstrap.min.css"
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <TodoInput />
+        <TodoList />
+      </div>
+    );
+  }
+ 
+}
+
+export default App;
